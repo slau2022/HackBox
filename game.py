4461362435f846a550ba38535cae5ef47a21d35e
VivereSV <32445070+VivereSV@users.noreply.github.com>
2018-09-21 21:07:43 -0400

    Update game.py
    black screen

--- game.py
+++ game.py
@@ -1,1 +1,25 @@
+import pygame as pg
 
+class HackBox():
+    def __init__(self):
+        pg.init()
+        width, height = 500, 500
+        self.screen = pg.display.set_mode((width, height))
+        pg.display.set_caption("Hackbox")
+        self.clock = pg.time.Clock()
+
+    def update(self):
+        self.clock.tick(60)
+
+        self.screen.fill(0)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+
+        pg.display.flip()
+
+
+hb = HackBox()
+while 1:
+    hb.update()
