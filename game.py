9103075e8d7cdaaf0e1d4263356ab79a16af1095
VivereSV <32445070+VivereSV@users.noreply.github.com>
2018-09-21 22:57:51 -0400

    Update game.py
    i should really learn git
    
    also input box is now a class so it's easier to make

--- game.py
+++ game.py
@@ -1,22 +1,76 @@
 import pygame as pg
 
+pg.init()
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.Font(None, 32)
+
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, c, text=''):
+        self.input_box = pg.Rect(x, y, w, h)
+        self.c = c
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.txt_surface2 = FONT.render(text, True, self.color)
+        self.active = False
+        self.log = list()
+        self.max_msg = 25;
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.input_box.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    self.log.append(self.text)
+                    if len(self.log) > 25:
+                        self.log.pop(0)
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                elif len(self.text) < 40:
+                    self.text += event.unicode
+
+    def update(self):
+        # Render the current text.
+        self.txt_surface = FONT.render(self.text[0:20], True, self.color)
+        self.txt_surface2 = FONT.render(self.text[20:40], True, self.color)
+        self.input_box.w = 300
+
+    def draw(self, screen):
+        if self.c:
+            screen.blit(self.txt_surface, (self.input_box.x + 5, self.input_box.y + 5))
+            screen.blit(self.txt_surface2, (self.input_box.x + 5, self.input_box.y + 37))
+            y = 0;
+            for msg in self.log:
+                msg_surface = FONT.render(msg[0:20], True, self.color)
+                msg_surface2 = FONT.render(msg[20:40], True, self.color)
+                screen.blit(msg_surface, (0, y))
+                if len(msg) > 20:
+                    y += 20
+                screen.blit(msg_surface2, (0, y))
+                y += 20
+            pg.draw.rect(screen, self.color, self.input_box, 2)
+
 
 class HackBox():
     def __init__(self):
-        pg.init()
         self.width, self.height = 600, 600
         self.screen = pg.display.set_mode((self.width, self.height))
         pg.display.set_caption("Hackbox")
-        self.font = pg.font.Font(None, 32)
         self.clock = pg.time.Clock()
-        self.input_box = pg.Rect(0, 536, 600, 64)
-        self.color_inactive = pg.Color('lightskyblue3')
-        self.color_active = pg.Color('dodgerblue2')
-        self.color = self.color_inactive
-        self.active = False
-        self.text = ''
-        self.log = list()
-        self.max_msg = 25;
+        self.chat_box = InputBox(0, 536, 600, 64, True)
+        self.input_boxes = [self.chat_box]
 
     def loadingScreen(self):
         pg.draw.rect(self.screen, 0, (50, 50, 100, 100), 0)
@@ -34,47 +88,19 @@ class HackBox():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 exit()
-            if event.type == pg.MOUSEBUTTONDOWN:
-                # If the user clicked on the input_box rect.
-                if self.input_box.collidepoint(event.pos):
-                    # Toggle the active variable.
-                    self.active = not self.active
-                else:
-                    self.active = False
-                # Change the current color of the input box.
-                self.color = self.color_active if self.active else self.color_inactive
-            if event.type == pg.KEYDOWN:
-                if self.active:
-                    if event.key == pg.K_RETURN:
-                        self.log.append(self.text)
-                        if len(self.log) > 25:
-                            self.log.pop(0)
-                        self.text = ''
-                    elif event.key == pg.K_BACKSPACE:
-                        self.text = self.text[:-1]
-                    elif len(self.text) < 40:
-                        self.text += event.unicode
-        # Render the current text.
-        txt_surface = self.font.render(self.text[0:20], True, self.color)
-        txt_surface2 = self.font.render(self.text[20:40], True, self.color)
-        self.input_box.w = self.width / 2
-        self.screen.blit(txt_surface, (self.input_box.x + 5, self.input_box.y + 5))
-        self.screen.blit(txt_surface2, (self.input_box.x + 5, self.input_box.y + 37))
-        y = 0;
-        for msg in self.log:
-            msg_surface = self.font.render(msg[0:20], True, self.color)
-            msg_surface2 = self.font.render(msg[20:40], True, self.color)
-            self.screen.blit(msg_surface, (0, y))
-            if len(msg) > 20:
-                y += 20
-            self.screen.blit(msg_surface2, (0, y))
-            y += 20
-        pg.draw.rect(self.screen, self.color, self.input_box, 2)
+            for box in self.input_boxes:
+                box.handle_event(event)
+
+        for box in self.input_boxes:
+            box.update()
+
         if pg.mouse.get_pressed()[0]:
             pos = pg.mouse.get_pos()
             pg.draw.rect(self.screen, (255, 0, 0), (pos[0] - 5, pos[1] - 5, 10, 10), 0)
 
         hb.loadingScreen()
+        for box in self.input_boxes:
+            box.draw(self.screen)
         pg.display.flip()
 
 
